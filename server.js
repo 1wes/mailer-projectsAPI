2e1e047586699c8a68e1a9bd752dae3ddb4d1570
1wes <okemwawes@gmail.com>
2024-03-26 15:37:47 +0300

    feat: add pithone mailing route

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const cors=require('cors');
 const { port }=require('./env-config');
 const projects=require('./projects');
 const sendMail=require('./mail');
-const project=require('./project');
+const project = require('./project');
+const pithoneMail = require('./pithone-mail');
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
@@ -13,6 +14,7 @@ app.use(cors({}));
 app.use("/", sendMail);
 app.use('/', projects);
 app.use('/', project);
+app.use('/', pithoneMail);
 
 app.listen(port, ()=>{
     console.log(`Server started at port ${port}`); 
