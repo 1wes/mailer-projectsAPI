41c38e001d0b7819f05296d1e85ee9cf4629d996
1wes <okemwawes@gmail.com>
2024-03-26 15:36:45 +0300

    feat: export pithone mailing info

--- env-config.js
+++ env-config.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
-const {PORT, EMAIL, PASSWORD}=process.env;
+const { PORT, EMAIL, PASSWORD, PITHONE_EMAIL, PITHONE_PASSWORD, HOST } = process.env;
 
 module.exports={
     port:PORT,
     myEmail:EMAIL,
-    pass:PASSWORD
+    pass: PASSWORD,
+    pithoneEmail: PITHONE_EMAIL,
+    pithonePassword: PITHONE_PASSWORD,
+    host:HOST
 }
