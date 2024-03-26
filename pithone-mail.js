2bc84f642299671b25fb9ac2a01d3ba211763740
1wes <okemwawes@gmail.com>
2024-03-26 15:38:25 +0300

    feat: send mail to info@pithone.co.ke

---
+++ pithone-mail.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const router = express.Router();
+const { pithoneEmail, pithonePassword, host } = require('./env-config');
+
+const transporter = nodemailer.createTransport({
+    host: host,
+    port: 465,
+    secure: true,
+    auth: {
+        user: pithoneEmail,
+        pass: pithonePassword
+    },
+    tls: {
+        rejectUnauthorized: false
+    }
+});
+
+transporter.verify(err=>{
+
+    if(err){
+        console.log(err);
+    }else{
+        console.log("mail server ready");
+    }
+});
+
+router.post("/pithone-mail", (req, res) => {
+
+    let { name, email, message } = req.body;
+
+    let mailOptions = {
+        from: email,
+        to: pithoneEmail,
+        subject: `Message From ${name}`,
+        text: message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        
+        if (error) {
+            res.sendStatus(503);
+        } else {
+            console.log("message sent", info.messageId)
+            res.sendStatus(200);
+        }
+    })
+});
+
+module.exports = router;
