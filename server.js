e5d5741b656f68a3f1fa6822b931e1f5b568d587
1wes <okemwawes@gmail.com>
2024-03-26 20:58:45 +0300

    fix: remove slash from origin urls to fix cors error in prod

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const pithoneMail = require('./pithone-mail');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:["http://localhost:5173/", "https://pithone-international.onrender.com/", "https://www.pithone.co.ke/"]
+    origin:["http://localhost:5173", "https://pithone-international.onrender.com", "https://www.pithone.co.ke"]
 }));
 
 app.use("/", sendMail);
