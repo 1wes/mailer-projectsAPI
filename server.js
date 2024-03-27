e9c333406e42adfa6340cfad52ffbbe510cc30b2
1wes <okemwawes@gmail.com>
2024-03-27 13:08:09 +0300

    refactor: shorten url

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const pithoneMail = require('./pithone-mail');
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:["http://localhost:5173", "https://pithone-international.onrender.com", "https://www.pithone.co.ke", "https://1wes.github.io", "https://okemwawes.onrender.com"]
+    origin:["http://localhost:5173", "https://pithone-international.onrender.com", "https://pithone.co.ke", "https://1wes.github.io", "https://okemwawes.onrender.com"]
 }));
 
 app.use("/", sendMail);
